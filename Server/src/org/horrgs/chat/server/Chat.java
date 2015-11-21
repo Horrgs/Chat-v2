142e68a46caa49b84aea33d9fd354f3ed8d6e137
Horrgs <horrgs34@gmail.com>
2015-11-21 17:59:50 -0500

    Update .gitignore and startup class for server.

---
+++ Server/src/org/horrgs/chat/server/Chat.java
@@ -0,0 +1,11 @@
+package org.horrgs.chat.server;
+
+/**
+ * Created by Horrgs on 11/21/2015.
+ */
+public class Chat {
+
+    public static void main(String[] args) {
+        //TODO: startup.
+    }
+}
